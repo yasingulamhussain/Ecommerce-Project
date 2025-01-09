1c0f496c14a3dbe6705f98fae6f21df2b2768203
yasingulamhussain <yasin.gulamhussain@thegymgroup.com>
2025-01-09 09:18:15 +0000

    First commit for react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="categories-container">
+      <div className="category">
+        {/* <img/> */}
+        
+      </div>
+      <div className="category-body-container">
+        <h2> Hats</h2>
+        <p>Shop now</p>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
