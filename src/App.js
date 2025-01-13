78d98d7eccf820b3a2c8ac4328c209502abd7184
Yasin <yasin.gulamhussain@thegymgroup.com>
2025-01-13 11:39:28 +0000

    Added react router, navigation component, and home component, relevant styling, links, and routes.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import './categories.styles.scss';
-import Directory from './components/directory/directory.component';
+import {Routes, Route, Outlet} from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/Navigation/navigation.component';
+
 
 
 const App=()=>{
+  return(
+    <Routes>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home/>}/>
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "footwear",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  ;
-
-  return (
-  <Directory categories={categories}/>
+      </Route>
+    </Routes>
   );
 }
 
