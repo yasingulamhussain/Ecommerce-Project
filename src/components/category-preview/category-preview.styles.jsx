f43275eba0dff050c6c22b13ddbeaa5df958b0f9
yasingulamhussain <yasin.gulamhussain@thegymgroup.com>
2025-02-06 08:51:43 +0000

    modified state management for cart and user using reducers. implemented useReducer hook in cart and user context. created reducer.utils.js to store all the reducers. Updated cart-item, category-preview, checkout-item, product-card, sign-in-form components to use the new state management. updated firebase.utils.js to use the new state management.

--- src/components/category-preview/category-preview.styles.jsx
+++ src/components/category-preview/category-preview.styles.jsx
@@ -1,18 +1,18 @@
-.category-preview-container {
+import styled from "styled-components";
+
+export const CategoryPreviewContainer = styled.div`
     display: flex;
     flex-direction: column;
     margin-bottom: 30px;
-  
-    .title {
+`
+export const Title = styled.h2`
       font-size: 28px;
       margin-bottom: 25px;
       cursor: pointer;
-    }
-  
-    .preview {
+`
+export const Preview = styled.div`
       display: grid;
       grid-template-columns: repeat(4, 1fr);
       column-gap: 20px;
-    }
-  }
-  
+`
+
